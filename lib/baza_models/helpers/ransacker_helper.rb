540fe5e800c675e09f04f1054f34a46c87860d17
kaspernj <k@spernj.org>
2016-02-04 14:24:33 +0100

    Fixed attribute name. Fixed desc sorting on second click

--- lib/baza_models/helpers/ransacker_helper.rb
+++ lib/baza_models/helpers/ransacker_helper.rb
@@ -2,10 +2,18 @@ module BazaModels::Helpers::RansackerHelper
   def bm_sort_link(ransacker, attribute)
     require "html_gen"
 
-    label = ransacker.klass.human_attribute_name(:attribute)
+    label = ransacker.klass.human_attribute_name(attribute)
 
     new_params = params.clone
-    new_params[:s] = "#{attribute} asc"
+    new_params[:q] ||= {}
+
+    sort_asc = "#{attribute} asc"
+
+    if new_params[:q][:s] == sort_asc
+      new_params[:q][:s] = "#{attribute} desc"
+    else
+      new_params[:q][:s] = sort_asc
+    end
 
     href = url_for(new_params)
 
