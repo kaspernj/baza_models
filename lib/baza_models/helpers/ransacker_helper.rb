0b83376ec694ae03f406790a2f026a749d93fa2a
kaspernj <kaspernj@gmail.com>
2016-05-05 13:00:48 +0200

    Added helper method for pagination content

--- lib/baza_models/helpers/ransacker_helper.rb
+++ lib/baza_models/helpers/ransacker_helper.rb
@@ -1,8 +1,46 @@
 module BazaModels::Helpers::RansackerHelper
-  def bm_sort_link(ransacker, attribute)
+  def bm_paginate_content(collection)
     require "html_gen"
 
-    label = ransacker.klass.human_attribute_name(attribute)
+    new_params = params.dup
+    current_page = collection.page
+    total_pages = collection.total_pages
+
+    container = HtmlGen::Element.new(:div)
+
+    if current_page > 1
+      new_params[:page] = current_page - 1
+      container.add_ele(:a, str: "Previous", attr: {href: url_for(new_params)})
+    else
+      container.add_ele(:span, str: "Previous")
+    end
+
+    1.upto(collection.total_pages) do |page_i|
+      new_params[:page] = page_i
+
+      link = container.add_ele(:a, attr: {href: url_for(new_params)})
+
+      if page_i == current_page
+        link.add_ele(:b, str: page_i.to_s)
+      else
+        link.add_str(page_i.to_s)
+      end
+    end
+
+    if current_page < total_pages
+      new_params[:page] = current_page + 1
+      container.add_ele(:a, str: "Next", attr: {href: url_for(new_params)})
+    else
+      container.add_ele(:span, str: "Next")
+    end
+
+    container.html
+  end
+
+  def bm_sort_link(ransacker, attribute, label = nil)
+    require "html_gen"
+
+    label = ransacker.klass.human_attribute_name(attribute) if label.to_s.strip.empty?
 
     new_params = params.clone
     new_params[:q] ||= {}
